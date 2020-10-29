8a11a749e34c1d9c6e5bc5737586fb3bca7db0e4
Yevhwnii <mirandoos3223@gmail.com>
2020-10-29 20:05:12 +0100

    side menu v.1

--- src/pages/Main/containers/SideMenu/index.tsx
+++ src/pages/Main/containers/SideMenu/index.tsx
@@ -1,69 +1,73 @@
 import React from 'react';
-import clsx from 'clsx';
 
-import Drawer from '@material-ui/core/Drawer';
-import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import MenuIcon from '@material-ui/icons/Menu';
+import ProfileIcon from '@material-ui/icons/PersonOutlineOutlined';
+import MapIcon from '@material-ui/icons/Map';
+import RestaurantIcon from '@material-ui/icons/Restaurant';
 
-const useStyles = makeStyles(() => ({
-  drawer: {
-    width: '100%',
-    height: '100%',
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: '100%',
-    position: 'relative',
-  },
-  hide: {
-    transform: 'translateX(100%)',
+import SideDrawer from '../../../../components/Drawer';
+import CustomizedListItem from '../../../../components/ListItem';
+import SearchBar from '../../../../components/SearchBar';
+
+const useSideMenuStyles = makeStyles(() => ({
+  header: {
+    padding: 20,
+    textAlign: 'center',
+    fontWeight: 700,
   },
 }));
 
 interface SideMenuProps {
   show: boolean;
+  onClose: () => void;
+  drawerVariant: 'permanent' | 'persistent' | 'temporary' | undefined;
+  smSmallScreen: boolean;
 }
 
-const SideMenu: React.FC<SideMenuProps> = ({ show }) => {
-  const classes = useStyles();
+const SideMenu: React.FC<SideMenuProps> = ({
+  show,
+  onClose,
+  drawerVariant,
+  smSmallScreen,
+}) => {
+  const classes = useSideMenuStyles();
+
   return (
-    <Drawer
-      className={clsx(classes.drawer, {
-        [classes.hide]: !show,
-      })}
-      variant='persistent'
-      anchor='left'
-      open={show}
-      classes={{
-        paper: classes.drawerPaper,
-      }}>
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <MenuIcon /> : <MenuIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+    <SideDrawer drawerVariant={drawerVariant} show={show} onClose={onClose}>
+      <List component='nav' aria-labelledby='side menu items'>
+        {smSmallScreen && (
+          <>
+            <Typography className={classes.header} variant='h6' noWrap>
+              Recommendation system
+            </Typography>
+            <Divider />
+          </>
+        )}
+
+        <SearchBar componentVariant='li' />
+
+        <CustomizedListItem
+          icon={<MapIcon />}
+          text='Map'
+          onClick={() => alert('Map')}
+        />
+
+        <CustomizedListItem
+          icon={<ProfileIcon />}
+          text='Profile'
+          onClick={() => alert('Profile')}
+        />
+
+        <CustomizedListItem
+          icon={<RestaurantIcon />}
+          text='Restaurants'
+          onClick={() => alert('Restaurants')}
+        />
       </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <MenuIcon /> : <MenuIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </Drawer>
+    </SideDrawer>
   );
 };
 
