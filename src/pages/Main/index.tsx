dd9f16eeff5c78ed7dda00e8970642d825af50a7
Yevhwnii <mirandoos3223@gmail.com>
2020-10-29 20:04:38 +0100

    side menu display logic

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,24 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Grid from '@material-ui/core/Grid';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import NavigationBar from './containers/AppBar';
 import SideMenu from './containers/SideMenu';
 
-const Main = () => {
+const Main: React.FC = () => {
   const [showSideMenu, setShowSideMenu] = useState<boolean>(true);
+  const [sideMenuStyles, setSideMenuStyles] = useState({});
+  const theme = useTheme();
+  const mdSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const smSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+  // If viewport is less than 950px, side menu automatically closes
+  useEffect(() => {
+    if (mdSmallScreen) {
+      setShowSideMenu(false);
+    } else {
+      setShowSideMenu(true);
+    }
+  }, [mdSmallScreen]);
+
+  // If viewport is less than 950px and user clicks on menu toggle, side menu becomes with position absolute and goes over main application
+  useEffect(() => {
+    if (!showSideMenu || (mdSmallScreen && showSideMenu)) {
+      setSideMenuStyles({
+        position: 'absolute',
+        height: '100%',
+      });
+    } else {
+      setSideMenuStyles({});
+    }
+  }, [mdSmallScreen, showSideMenu]);
 
   const handleDrawerToogle = () => {
     setShowSideMenu(!showSideMenu);
   };
+
   return (
     <Grid container>
       <Grid item xs={12}>
-        <NavigationBar onDrawerToggle={handleDrawerToogle} />
+        <NavigationBar
+          smSmallScreen={smSmallScreen}
+          onDrawerToggle={handleDrawerToogle}
+        />
       </Grid>
       <Grid container style={{ height: 'calc(100vh - 64px)' }} item xs={12}>
-        <Grid style={showSideMenu ? {} : { display: 'none' }} item xs={2}>
-          <SideMenu show={showSideMenu} />
+        <Grid style={sideMenuStyles} item xs={2}>
+          <SideMenu
+            drawerVariant={mdSmallScreen ? 'temporary' : 'persistent'}
+            smSmallScreen={smSmallScreen}
+            show={showSideMenu}
+            onClose={handleDrawerToogle}
+          />
         </Grid>
         <Grid item xs={showSideMenu ? 10 : 12}>
           <h1>Application</h1>
