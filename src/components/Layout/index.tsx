260819eb27d42601460a6182e51c8df1697913fc
Yevhwnii <mirandoos3223@gmail.com>
2020-11-01 17:22:42 +0100

    responsive layout for main part of application

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,55 @@
+import React, { useContext, useEffect, useState } from 'react';
+
+import Grid from '@material-ui/core/Grid';
+
+import { MediaContext } from '../../common/context/mediaContext';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+interface IDisplayConfig {
+  side: boolean | 1 | 2;
+  main: 8 | 10 | 12;
+}
+
+// Layout component for main part of application
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const [displayConfig, setDisplayConfig] = useState<IDisplayConfig>({
+    side: 2,
+    main: 8,
+  });
+
+  const media = useContext(MediaContext);
+
+  useEffect(() => {
+    if (media.xsSmallScreen) {
+      setDisplayConfig({
+        side: false,
+        main: 12,
+      });
+    } else if (media.mdSmallScreen) {
+      setDisplayConfig({
+        side: 1,
+        main: 10,
+      });
+    } else {
+      setDisplayConfig({
+        side: 2,
+        main: 8,
+      });
+    }
+  }, [media]);
+
+  return (
+    <Grid container style={{ height: '100%' }}>
+      <Grid item xs={displayConfig.side} />
+      <Grid item xs={displayConfig.main}>
+        {children}
+      </Grid>
+      <Grid item xs={displayConfig.side} />
+    </Grid>
+  );
+};
+
+export default Layout;
