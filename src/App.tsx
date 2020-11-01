2d6c09e25b60a0331e8281239d992f2e9e792183
Yevhwnii <mirandoos3223@gmail.com>
2020-11-01 17:21:45 +0100

    provider for media context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 import Main from './pages/Main';
+import { MediaContext } from './common/context/mediaContext';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 function App() {
+  const theme = useTheme();
   return (
     <div className='App'>
-      <Main />
+      <MediaContext.Provider
+        value={{
+          mdSmallScreen: useMediaQuery(theme.breakpoints.down('md')),
+          smSmallScreen: useMediaQuery(theme.breakpoints.down('sm')),
+          xsSmallScreen: useMediaQuery(theme.breakpoints.down('xs')),
+        }}>
+        <Main />
+      </MediaContext.Provider>
     </div>
   );
 }
