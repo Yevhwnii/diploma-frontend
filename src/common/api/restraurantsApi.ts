080da3fcb8a7903938be20cf0199532f3cbee156
Yevhwnii <mirandoos3223@gmail.com>
2020-11-04 19:27:43 +0100

    geocoordinate google api request

--- src/common/api/restraurantsApi.ts
+++ src/common/api/restraurantsApi.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { trimAddress } from '../utils/addressTrim';
 
 export interface IRestaurant {
   id: number;
@@ -26,6 +27,11 @@ export interface MenuItem {
   ];
 }
 
+export interface ILocation {
+  lat: number;
+  lng: number;
+}
+
 export const RestrauntsApi = {
   getAll: async (): Promise<IRestaurant[]> => {
     const response = await axios.get('/restaurants');
@@ -35,5 +41,14 @@ export const RestrauntsApi = {
   getSingle: async (id: string): Promise<IRestaurant> => {
     const response = await axios.get(`/restaurants/${id}`);
     return response.data;
+  },
+  getRestaurantLocation: async (
+    restaurant: IRestaurant
+  ): Promise<ILocation> => {
+    const address = trimAddress(restaurant.address);
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
+    );
+    return response.data.results[0].geometry.location;
   },
 };
