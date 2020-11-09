d6e2413350bc2ae9f7e987f72ff5f8f4f4aa06db
Yevhwnii <mirandoos3223@gmail.com>
2020-11-09 17:17:21 +0100

    paper component

---
+++ src/components/PaperLayout/index.tsx
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import { IMedia, MediaContext } from '../../common/context/mediaContext';
+
+const usePaperLayoutStyles = makeStyles(() => ({
+  paper: (media: IMedia) => ({
+    padding: 10,
+    marginTop: media.xsSmallScreen ? 0 : 25,
+    height: media.xsSmallScreen ? '100%' : '70%',
+    borderRadius: 15,
+    boxShadow: ' 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
+    display: 'flex',
+    flexDirection: 'column',
+  }),
+}));
+
+interface PaperProps {
+  children: React.ReactNode;
+}
+
+const PaperLayout: React.FC<PaperProps> = ({ children }) => {
+  const media = useContext(MediaContext);
+  const classes = usePaperLayoutStyles(media);
+
+  return (
+    <Paper className={classes.paper} elevation={2}>
+      {children}
+    </Paper>
+  );
+};
+
+export default PaperLayout;
