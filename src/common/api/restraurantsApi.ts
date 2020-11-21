aec442643b2d2a90b4d49efbc9df8f0326713261
Yevhwnii <mirandoos3223@gmail.com>
2020-11-21 19:51:20 +0100

    fixes + create method

--- src/common/api/restraurantsApi.ts
+++ src/common/api/restraurantsApi.ts
@@ -2,16 +2,15 @@ import axios from 'axios';
 import { trimAddress } from '../utils/addressTrim';
 
 export interface IRestaurant {
-  id: number;
   _id: string;
   name: string;
   address: string;
   location?: ILocation;
   description: string;
   tags: string;
-  webSite: string;
+  website: string;
   imageUrl: string;
-  menu?: {
+  menu: {
     _id: string;
     items: MenuItem[];
   };
@@ -19,19 +18,28 @@ export interface IRestaurant {
 
 export interface MenuItem {
   category: string;
-  menuItems: [
-    {
-      id: string;
-      name: string;
-      price: string;
-    }
-  ];
+  meals: Meal[];
+}
+
+export interface Meal {
+  name: string;
+  price: number;
 }
 
 export interface ILocation {
   lat: number;
   lng: number;
 }
+
+const getRestaurantLocation = async (
+  addressInput: string
+): Promise<ILocation> => {
+  const address = trimAddress(addressInput);
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
+  );
+  return response.data.results[0].geometry.location;
+};
 
 export const RestrauntsApi = {
   getAll: async (): Promise<IRestaurant[]> => {
@@ -51,5 +59,43 @@ export const RestrauntsApi = {
       `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
     );
     return response.data.results[0].geometry.location;
+  },
+  getAllDb: async (): Promise<IRestaurant[]> => {
+    const response = await axios.get('http://localhost:4000/restaurants');
+    const restaurants: IRestaurant[] = response.data;
+    return restaurants;
+  },
+  getSingleDb: async (id: string): Promise<IRestaurant> => {
+    const response = await axios.get(`http://localhost:4000/restaurants/${id}`);
+    return response.data;
+  },
+  create: async (restaurant: any): Promise<void> => {
+    const location = await getRestaurantLocation(restaurant.address);
+    console.log(location);
+
+    try {
+      const response = await axios.post('http://localhost:4000/restaurants', {
+        name: restaurant.name,
+        description: restaurant.description,
+        imageUrl: restaurant.imageUrl,
+        address: restaurant.address,
+        location: location,
+        tags: restaurant.tags,
+        website: restaurant.website,
+        menu: {
+          items: [...restaurant.menu.items],
+        },
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+  delete: async (id: string): Promise<void> => {
+    try {
+      await axios.delete(`http://localhost:4000/restaurants/${id}`);
+    } catch (error) {
+      console.log(error.message);
+    }
   },
 };
