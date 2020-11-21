2bb26a2146511f37ea24d626e5bddd11d955743c
Yevhwnii <mirandoos3223@gmail.com>
2020-11-21 19:54:23 +0100

    Admin page

---
+++ src/pages/Admin/index.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react';
+
+import { IRestaurant, RestrauntsApi } from '../../common/api/restraurantsApi';
+import Layout from '../../components/Layout';
+import PaperLayout from '../../components/PaperLayout';
+import AdminRestaurant from '../../components/Restaurant/AdminPanelRestaurant';
+import Spinner from '../../components/Spinner';
+
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import AddIcon from '@material-ui/icons/Add';
+import List from '@material-ui/core/List';
+import { useHistory } from 'react-router-dom';
+
+const useAdminStyles = makeStyles(() => ({
+  header: {
+    width: '100%',
+    height: '10%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    '& p': {
+      fontSize: '1.3rem',
+      fontWeight: 600,
+    },
+    '& button': {
+      color: 'rgb(195, 82, 44)',
+    },
+  },
+}));
+
+const Admin = () => {
+  const classes = useAdminStyles();
+  const history = useHistory();
+  const [restaurants, setRestaurants] = useState<IRestaurant[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const onDeleteHandler = async (id: string, index: number) => {
+    const allRestaurants = [...restaurants];
+    allRestaurants.splice(index, 1);
+    setRestaurants(allRestaurants);
+    await RestrauntsApi.delete(id);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    const getResponse = async () => {
+      const response = await RestrauntsApi.getAllDb();
+      setRestaurants(response);
+      setLoading(false);
+    };
+
+    getResponse();
+  }, []);
+
+  return (
+    <Layout>
+      <PaperLayout>
+        <div className={classes.header}>
+          <Typography>Manage restaurants</Typography>
+          <Button
+            onClick={() => history.push(`/admin/new`)}
+            startIcon={<AddIcon />}
+            variant='text'>
+            Add new restaurant
+          </Button>
+        </div>
+        <Divider />
+        {!loading ? (
+          restaurants?.map((restaurant, index) => {
+            return (
+              <List>
+                <AdminRestaurant
+                  restaurant={restaurant}
+                  restaurantIndex={index}
+                  onDelete={onDeleteHandler}
+                />
+              </List>
+            );
+          })
+        ) : (
+          <Spinner />
+        )}
+      </PaperLayout>
+    </Layout>
+  );
+};
+
+export default Admin;
