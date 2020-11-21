01ad751727ee54253a01e120d34b77bc05af98f3
Yevhwnii <mirandoos3223@gmail.com>
2020-11-21 19:53:32 +0100

    maxHeight for paperlayout

--- src/components/PaperLayout/index.tsx
+++ src/components/PaperLayout/index.tsx
@@ -6,12 +6,14 @@ import { IMedia, MediaContext } from '../../common/context/mediaContext';
 const usePaperLayoutStyles = makeStyles(() => ({
   paper: (media: IMedia) => ({
     padding: 10,
+    position: 'relative',
     marginTop: media.xsSmallScreen ? 0 : 25,
     height: media.xsSmallScreen ? '100%' : '70%',
     borderRadius: 15,
     boxShadow: ' 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
     display: 'flex',
     flexDirection: 'column',
+    maxHeight: '90vh',
   }),
 }));
 
