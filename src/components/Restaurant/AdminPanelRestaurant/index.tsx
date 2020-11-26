b03a4fab078caf6a1c177b0bb04882f3c761b575
Yevhwnii <mirandoos3223@gmail.com>
2020-11-26 22:25:47 +0100

    reworked admin restaurants (more flexible)

--- src/components/Restaurant/AdminPanelRestaurant/index.tsx
+++ src/components/Restaurant/AdminPanelRestaurant/index.tsx
@@ -11,29 +11,50 @@ import { useHistory } from 'react-router-dom';
 
 interface AdminRestaurantProps {
   restaurant: IRestaurant;
-  onDelete: (id: string, index: number) => void;
-  restaurantIndex: number;
+  onDelete?: (id: string, index: number) => void;
+  restaurantIndex?: number;
+  withDelete?: boolean;
 }
 
 const AdminRestaurant: React.FC<AdminRestaurantProps> = ({
   restaurant,
-  onDelete,
-  restaurantIndex,
+  onDelete = () => {},
+  restaurantIndex = 0,
+  withDelete = true,
 }) => {
   const history = useHistory();
 
-  return (
-    <ListItem
-      onClick={() => history.push(`/restaurants/${restaurant._id}`)}
-      button>
-      <ListItemText primary={restaurant.name} secondary={restaurant.address} />
-      <ListItemSecondaryAction>
-        <IconButton onClick={() => onDelete(restaurant._id, restaurantIndex)}>
-          <DeleteIcon />
-        </IconButton>
-      </ListItemSecondaryAction>
-    </ListItem>
-  );
+  if (withDelete) {
+    return (
+      <ListItem
+        onClick={() => history.push(`/restaurants/${restaurant._id}`)}
+        button>
+        <ListItemText
+          primary={restaurant.name}
+          secondary={restaurant.address}
+        />
+        <ListItemSecondaryAction>
+          {withDelete && (
+            <IconButton
+              onClick={() => onDelete(restaurant._id, restaurantIndex)}>
+              <DeleteIcon />
+            </IconButton>
+          )}
+        </ListItemSecondaryAction>
+      </ListItem>
+    );
+  } else {
+    return (
+      <ListItem
+        onClick={() => history.push(`/restaurants/${restaurant._id}`)}
+        button>
+        <ListItemText
+          primary={restaurant.name}
+          secondary={restaurant.address}
+        />
+      </ListItem>
+    );
+  }
 };
 
 export default AdminRestaurant;
